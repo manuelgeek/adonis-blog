06d22e4638ffa9b54bf0ec24f85610c44366baa8
Magak Emmanuel <emashmagak@gmail.com>
2021-11-05 11:58:24 +0300

    fixed middleware issue

--- app/Middleware/Guest.ts
+++ app/Middleware/Guest.ts
@@ -3,10 +3,10 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 export default class Guest {
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
     // code for middleware goes here. ABOVE THE NEXT CALL
-    if (await !auth.use('web').check()) {
-      await next()
-    } else {
+    if (await auth.use('web').check()) {
       response.redirect().toPath('/')
     }
+
+    await next()
   }
 }
